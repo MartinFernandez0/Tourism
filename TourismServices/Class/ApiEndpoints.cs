786d136385d902dc797580bce1f6d25071ad0da2
lautaroperesin <lautaroperesin@gmail.com>
2024-11-26 00:18:58 -0300

    Mostrando destinos

--- TourismServices/Class/ApiEndpoints.cs
+++ TourismServices/Class/ApiEndpoints.cs
@@ -15,13 +15,13 @@ namespace TourismServices.Class
         {
             return name switch
             {
-                nameof(pfActivity) => "api/pfActivity",
-                nameof(pfAdministrator) => "api/pfAdministrator",
-                nameof(pfDestination) => "api/pfDestination",
-                nameof(pfItinerary) => "api/pfItinerary",
-                nameof(pfReservation) => "api/pfReservation",
-                nameof(pfTransaction) => "api/pfTransaction",
-                nameof(pfClient) => "api/pfClient",
+                nameof(pfActivity) => pfActivity,
+                nameof(pfAdministrator) => pfAdministrator,
+                nameof(pfDestination) => pfDestination,
+                nameof(pfItinerary) => pfItinerary,
+                nameof(pfReservation) => pfReservation,
+                nameof(pfTransaction) => pfTransaction,
+                nameof(pfClient) => pfClient,
 
                 _ => throw new ArgumentException($"Endpoint '{name}' no está definido.")
             };
